9000528be49c0125c340c29e7a42226c99b99b80
andy.yeung <andy.yeung@travelzen.com>
2012-11-06 11:46:00 +0800

    Update schema

--- sql/church.sql
+++ sql/church.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 02, 2012 at 12:53 AM
--- Server version: 5.5.25a
--- PHP Version: 5.4.4
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2012 at 04:44 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,53 +30,90 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `id` bigint(19) NOT NULL AUTO_INCREMENT,
   `user_profile_id` bigint(20) NOT NULL,
   `date` date NOT NULL,
-  `arrival_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `record_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=91 ;
+
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `attendance`
+-- Table structure for table `district`
 --
 
-INSERT INTO `attendance` (`id`, `user_profile_id`, `date`, `arrival_time`) VALUES
-(1, 1, '2012-11-05', '2012-11-01 15:50:21'),
-(2, 2, '2012-11-02', '2012-11-01 16:22:13'),
-(3, 7, '2012-11-02', '2012-11-01 16:22:22'),
-(4, 1, '2012-11-02', '2012-11-01 16:22:25'),
-(5, 1, '2012-11-02', '2012-11-01 16:22:59'),
-(6, 1, '2012-11-02', '2012-11-01 16:23:20'),
-(7, 3, '2012-11-02', '2012-11-01 16:26:49'),
-(8, 15, '2012-11-02', '2012-11-01 16:26:56'),
-(9, 15, '2012-11-02', '2012-11-01 16:26:56'),
-(10, 1524, '2012-11-02', '2012-11-01 16:27:14'),
-(11, 1524, '2012-11-02', '2012-11-01 16:27:14'),
-(12, 22222, '2012-11-02', '2012-11-01 16:28:00'),
-(13, 22222, '2012-11-02', '2012-11-01 16:28:00'),
-(14, 22222, '2012-11-02', '2012-11-01 16:28:14'),
-(15, 22222, '2012-11-02', '2012-11-01 16:28:14'),
-(16, 1, '2012-11-02', '2012-11-01 16:28:20'),
-(17, 22222, '2012-11-02', '2012-11-01 16:28:26'),
-(18, 22222, '2012-11-02', '2012-11-01 16:28:26'),
-(19, 22222, '2012-11-02', '2012-11-01 16:28:44'),
-(20, 22222, '2012-11-02', '2012-11-01 16:28:46'),
-(21, 22222, '2012-11-02', '2012-11-01 16:28:46'),
-(22, 22222, '2012-11-02', '2012-11-01 16:28:51'),
-(23, 22222, '2012-11-02', '2012-11-01 16:28:51'),
-(24, 22222, '2012-11-02', '2012-11-01 16:34:08'),
-(25, 6, '2012-11-02', '2012-11-01 16:34:12'),
-(26, 6, '2012-11-02', '2012-11-01 16:34:31'),
-(27, 5, '2012-11-02', '2012-11-01 16:34:33'),
-(28, 8, '2012-11-02', '2012-11-01 16:34:58'),
-(29, 3, '2012-11-02', '2012-11-01 16:40:00'),
-(30, 5, '2012-11-02', '2012-11-01 16:40:31'),
-(31, 3, '2012-11-02', '2012-11-01 16:43:23'),
-(32, 3, '2012-11-02', '2012-11-01 16:43:49'),
-(33, 3, '2012-11-02', '2012-11-01 16:45:35'),
-(34, 5, '2012-11-02', '2012-11-01 16:45:40'),
-(35, 5, '2012-11-02', '2012-11-01 16:46:32'),
-(36, 3, '2012-11-02', '2012-11-01 16:46:42'),
-(37, 3, '2012-11-02', '2012-11-01 16:46:56'),
-(38, 3, '2012-11-02', '2012-11-01 16:47:34');
+CREATE TABLE IF NOT EXISTS `district` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `code` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `code` (`code`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `district`
+--
+
+INSERT INTO `district` (`id`, `name`, `code`) VALUES
+(1, 'Tsim Sha Tsui', '01'),
+(2, 'Yuen Long', '02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_group_mapping`
+--
+
+CREATE TABLE IF NOT EXISTS `profile_group_mapping` (
+  `user_profile_id` bigint(20) NOT NULL,
+  `user_group_id` bigint(20) NOT NULL,
+  KEY `user_profile_id` (`user_profile_id`,`user_group_id`),
+  KEY `user_group_id` (`user_group_id`),
+  KEY `user_profile_id_2` (`user_profile_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_district`
+--
+
+CREATE TABLE IF NOT EXISTS `sub_district` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `district_id` bigint(20) NOT NULL,
+  `code` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `district_id` (`district_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `sub_district`
+--
+
+INSERT INTO `sub_district` (`id`, `name`, `district_id`, `code`) VALUES
+(1, 'Kowloon City', 1, '01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`id`, `name`) VALUES
+(1, '職青');
 
 -- --------------------------------------------------------
 
@@ -86,16 +123,47 @@ INSERT INTO `attendance` (`id`, `user_profile_id`, `date`, `arrival_time`) VALUE
 
 CREATE TABLE IF NOT EXISTS `user_profile` (
   `id` bigint(19) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `login` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `chi_name` varchar(50) NOT NULL,
+  `eng_name` varchar(100) NOT NULL,
+  `sex` char(1) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `address` varchar(1000) DEFAULT NULL,
+  `district_id` bigint(20) NOT NULL,
+  `sub_district_id` bigint(20) NOT NULL,
+  `mobile_phone_no` varchar(20) DEFAULT NULL,
+  `last_update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_update_by` bigint(20) NOT NULL,
+  `card_id` int(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `district_id` (`district_id`),
+  KEY `sub_district_id` (`sub_district_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user_profile`
+-- Constraints for dumped tables
 --
 
-INSERT INTO `user_profile` (`id`, `name`) VALUES
-(1, '小明');
+--
+-- Constraints for table `profile_group_mapping`
+--
+ALTER TABLE `profile_group_mapping`
+  ADD CONSTRAINT `profile_group_mapping_ibfk_2` FOREIGN KEY (`user_group_id`) REFERENCES `user_group` (`id`),
+  ADD CONSTRAINT `profile_group_mapping_ibfk_1` FOREIGN KEY (`user_profile_id`) REFERENCES `user_profile` (`id`);
+
+--
+-- Constraints for table `sub_district`
+--
+ALTER TABLE `sub_district`
+  ADD CONSTRAINT `sub_district_ibfk_1` FOREIGN KEY (`district_id`) REFERENCES `district` (`id`);
+
+--
+-- Constraints for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD CONSTRAINT `user_profile_ibfk_2` FOREIGN KEY (`sub_district_id`) REFERENCES `sub_district` (`id`),
+  ADD CONSTRAINT `user_profile_ibfk_1` FOREIGN KEY (`district_id`) REFERENCES `district` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
